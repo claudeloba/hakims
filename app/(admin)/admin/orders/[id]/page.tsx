bce25fb50eed45320388c4fe7f6432af7273510e
Claude-Marcel Loba <100008570+claudeloba@users.noreply.github.com>
2024-06-06 18:21:29 +0200

    tog bort user table ska ta id direkt från clerk

--- app/(admin)/admin/orders/[id]/page.tsx
+++ app/(admin)/admin/orders/[id]/page.tsx
@@ -1,55 +1,71 @@
-import { Badge } from '@/components/reusables/badge'
-import { DescriptionDetails, DescriptionList, DescriptionTerm } from '@/components/reusables/description-list'
-import { Divider } from '@/components/reusables/divider'
-import { Heading, Subheading } from '@/components/reusables/heading'
-import { Link } from '@/components/reusables/link'
-import { BanknotesIcon, CalendarIcon, ChevronLeftIcon, CreditCardIcon } from '@heroicons/react/16/solid'
-import type { Metadata } from 'next'
-import { notFound } from 'next/navigation'
-import { getOrder } from '../../../../../queries/queries'
-import { formatter } from '../../../../../utils/formatter'
-import UpdateStatusButton from '../../../../../components/UpdateStatusButton'
+import { Badge } from "@/components/reusables/badge";
+import {
+  DescriptionDetails,
+  DescriptionList,
+  DescriptionTerm,
+} from "@/components/reusables/description-list";
+import { Divider } from "@/components/reusables/divider";
+import { Heading, Subheading } from "@/components/reusables/heading";
+import { Link } from "@/components/reusables/link";
+import {
+  BanknotesIcon,
+  CalendarIcon,
+  ChevronLeftIcon,
+  CreditCardIcon,
+} from "@heroicons/react/16/solid";
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { getOrder } from "../../../../../queries/queries";
+import { formatter } from "../../../../../utils/formatter";
+import UpdateStatusButton from "../../../../../components/UpdateStatusButton";
 
-export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
-  let [order] = await getOrder(parseInt(params.id))
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  let [order] = await getOrder(parseInt(params.id));
 
   return {
-    title: order && `Order #${order.orders.id}`,
-  }
+    title: order && `Order #${order.id}`,
+  };
 }
 
 export default async function Order({ params }: { params: { id: string } }) {
-  let [order] = await getOrder(parseInt(params.id))
+  let [order] = await getOrder(parseInt(params.id));
 
   if (!order) {
-    notFound()
+    notFound();
   }
-
-
 
   return (
     <>
       <div className="max-lg:hidden">
-        <Link href="/admin/orders" className="inline-flex items-center gap-2 text-sm/6 text-zinc-500 dark:text-zinc-400">
+        <Link
+          href="/admin/orders"
+          className="inline-flex items-center gap-2 text-sm/6 text-zinc-500 dark:text-zinc-400"
+        >
           <ChevronLeftIcon className="size-4 fill-zinc-400 dark:fill-zinc-500" />
           Ordrar
         </Link>
       </div>
       <div className="mt-4 lg:mt-8">
         <div className="flex items-center gap-4">
-          <Heading>Order #{order.orders.id}</Heading>
-          <Badge color={order.orders.status === "delivered" ? "lime" : "purple"}>{order.orders.status}</Badge>
+          <Heading>Order #{order.id}</Heading>
+          <Badge color={order.status === "delivered" ? "lime" : "purple"}>
+            {order.status}
+          </Badge>
         </div>
         <div className="isolate mt-2.5 flex flex-wrap justify-between gap-x-6 gap-y-4">
           <div className="flex flex-wrap gap-x-10 gap-y-4 py-1.5">
             <span className="flex items-center gap-3 text-base/6 text-zinc-950 sm:text-sm/6 dark:text-white">
               <BanknotesIcon className="size-4 shrink-0 fill-zinc-400 dark:fill-zinc-500" />
-              <span>{formatter.format(order.orders.totalPrice)}</span>
+              <span>{formatter.format(order.totalPrice)}</span>
             </span>
             <span className="flex items-center gap-3 text-base/6 text-zinc-950 sm:text-sm/6 dark:text-white">
               <CreditCardIcon className="size-4 shrink-0 fill-zinc-400 dark:fill-zinc-500" />
               <span className="inline-flex gap-3">
-              Mastercard
+                Mastercard
                 <span>
                   <span aria-hidden="true">••••</span> 4242
                 </span>
@@ -57,11 +73,15 @@ export default async function Order({ params }: { params: { id: string } }) {
             </span>
             <span className="flex items-center gap-3 text-base/6 text-zinc-950 sm:text-sm/6 dark:text-white">
               <CalendarIcon className="size-4 shrink-0 fill-zinc-400 dark:fill-zinc-500" />
-              <span>{order.orders.createdAt?.toDateString()}</span>
+              <span>{order.createdAt?.toDateString()}</span>
             </span>
           </div>
           <div className="flex gap-4">
-            {order.orders.status === "processing" ? <UpdateStatusButton id={parseInt(params.id)}/> : "" }
+            {order.status === "processing" ? (
+              <UpdateStatusButton id={parseInt(params.id)} />
+            ) : (
+              ""
+            )}
           </div>
         </div>
       </div>
@@ -70,11 +90,15 @@ export default async function Order({ params }: { params: { id: string } }) {
         <Divider className="mt-4" />
         <DescriptionList>
           <DescriptionTerm>Kund</DescriptionTerm>
-          <DescriptionDetails>{order.users?.username || "Anonym handlare"}</DescriptionDetails>
+          <DescriptionDetails>
+            {order.userId || "Anonym handlare"}
+          </DescriptionDetails>
           <DescriptionTerm>Summa</DescriptionTerm>
-          <DescriptionDetails>{formatter.format(order.orders.totalPrice)}</DescriptionDetails>
+          <DescriptionDetails>
+            {formatter.format(order.totalPrice)}
+          </DescriptionDetails>
         </DescriptionList>
       </div>
     </>
-  )
+  );
 }
